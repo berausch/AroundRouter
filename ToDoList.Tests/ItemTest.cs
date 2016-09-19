8146c838539db8a8990b10b1b327b156a0b5712d
Shradha Pulla <pullashradha@gmail.com>
2016-09-19 14:02:36 -0700

    Add unit testing part 1

---
+++ ToDoList.Tests/ItemTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Models;
+using Xunit;
+
+namespace ToDoList.Tests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            var item = new Item();
+            item.Description = "Wash the Parakeet";
+            var result = item.Description;
+            Assert.Equal("Wash the Parakeet", result);
+        }
+    }
+}
